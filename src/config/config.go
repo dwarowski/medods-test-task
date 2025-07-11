c94d321110719bbec00c46df2c1218ae8a70a760
Dwarowski <realcarenthusiast@yandex.ru>
2025-07-11 14:33:43 +0300

    refactor: small comments

--- src/config/config.go
+++ src/config/config.go
@@ -8,14 +8,17 @@ import (
 	"gorm.io/gorm"
 )
 
+// Connection to database via environment variable
 func ConnectToDatabase() (*gorm.DB, error) {
+
+	// Try getting env
 	dbc := os.Getenv("DATABASE_CONFIG")
 	if dbc == "" {
 		return nil, fmt.Errorf("DATABASE_CONFIG environment variable not set")
 	}
 
+	// Try connecting to db
 	db, err := gorm.Open(postgres.Open(dbc), &gorm.Config{})
-
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
